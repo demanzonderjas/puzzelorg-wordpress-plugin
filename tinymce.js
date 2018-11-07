f49794f7c11e936a6faabcec219f6fd4ec3d9ae0
Daan Weustenraad <weustenraad@gmail.com>
2018-11-07 17:33:50 +0100

    add puzzle to visual editor

--- tinymce.js
+++ tinymce.js
@@ -2,6 +2,22 @@
     var puzzles = JSON.parse(my_plugin.puzzles);
     var puzzlesArr = Object.keys(puzzles).map(function(key) { return puzzles[key] });
     console.log(puzzles)
+
+    function replaceGalleryShortcodes( content ) {
+        console.log("replacing...")
+		return content.replace( /\[puzzelorg([^\]]*)\]/g, function( match ) {
+			return html( 'wp-gallery', match );
+		});
+    }
+    
+	function html( cls, data ) {
+        console.log(cls, data)
+        var key = data.match(/key="(.*)"/)[1];
+		data = window.encodeURIComponent( data );
+		return "<div><iframe src='https://puzzel.org/nl/crossword/embed?p=" + key + "' width='750' height='800' frameborder='0'></iframe></div>";
+	}
+
+
     /* Register the buttons */
     tinymce.create('tinymce.plugins.PuzzelorgButtons', {
          init : function(ed, url) {
@@ -49,6 +65,21 @@
                    return_text = '<h1>' + selected_text + '</h1>';
                    ed.execCommand('mceInsertContent', 0, return_text);
               });
+
+            ed.on( 'BeforeSetContent', function( event ) {
+                console.log("before set content");
+                // 'wpview' handles the gallery shortcode when present
+                console.log(ed.plugins.wpview)
+                console.log(wp.mce)
+                event.content = replaceGalleryShortcodes( event.content );
+            });
+        
+            ed.on( 'PostProcess', function( event ) {
+                console.log("post processing");
+                if ( event.get ) {
+                    //event.content = restoreMediaShortcodes( event.content );
+                }
+            });
          },
          createControl : function(n, cm) {
               return null;
